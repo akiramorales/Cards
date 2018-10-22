f01a5a110b437ac5d654264b9887b4a9d37e7219
Akira Morales <akira.k.morales@gmail.com>
2018-10-22 13:34:45 -0400

    Add files via upload

---
+++ src/Cards.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ * @author Akira
+ *
+ */
+public class Cards
+{
+	private String suit;
+	private int value;
+	
+	public Cards(String suitIn, int valueIn)
+	{
+		suit = suitIn;
+		value = valueIn;
+	}
+	public Cards(String suitIn, char valueIn)
+	{
+		suit = suitIn;
+		value = valueIn;
+	}
+	
+}
