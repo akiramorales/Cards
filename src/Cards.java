b1b1b88cb014cd8c73a98af99676afde6c54305b
Akira Morales <akira.k.morales@gmail.com>
2018-10-23 13:13:01 -0400

    Add files via upload

--- src/Cards.java
+++ src/Cards.java
@@ -1,22 +1,55 @@
 /**
  * 
  * @author Akira
- *
+ * Card Class
+ * Period 6
+ * 
  */
 public class Cards
 {
-	private String suit;
+	//Fields
+	private char suit;
+	private char rank;
 	private int value;
 	
-	public Cards(String suitIn, int valueIn)
+	//Constructors
+	/**
+	 * Constructs a card
+	 * @param suitIn	initial suit
+	 * @param rankIn	initial rank
+	 * @param valueIn	initial value
+	 */
+	public Cards(char suitIn, char rankIn, int valueIn)
 	{
 		suit = suitIn;
-		value = valueIn;
-	}
-	public Cards(String suitIn, char valueIn)
-	{
-		suit = suitIn;
+		rank = rankIn;
 		value = valueIn;
 	}
 	
+	//Methods
+	/**
+	 * returns the suit of the card
+	 * @return suit		the suit of the card
+	 */
+	public int getSuit()
+	{
+		return suit;
+	}
+	/**
+	 * returns the rank of the card
+	 * @return rank		the rank of the card
+	 */
+	public int getRank()
+	{
+		return rank;
+	}
+	/**
+	 * returns the value of the card
+	 * @return value	the value of the card
+	 */
+	public int getValue()
+	{
+		return value;
+	}
+		
 }
